4777ebf5a9c597e9a05e0878fe601be4866afa43
Jonathan Seguin <jo@jonathanseguin.com>
2015-02-16 15:13:17 -0500

    Cache dot bracket results from PDB2DB

--- main.py
+++ main.py
@@ -19,12 +19,23 @@ for pdb_id in pdb_ids:
     pdb_file = pdb_id + ".pdb"
     pdb_path = TMP_DIR + pdb_file
     if not os.path.isfile(pdb_path):
-        print "Downloading " + pdb_file + " to tmp/" + pdb_file
+        print "Downloading " + pdb_file + " to " + pdb_path
         urllib.urlretrieve("http://www.rcsb.org/pdb/download/downloadFile.do?fileFormat=pdb&compression=NO&structureId=" + pdb_id, pdb_path)
+    else:
+        print "Loading PDB from " + pdb_path
 
-    print "Converting " + pdb_path + " to dot bracket"
-    pdb2db = PDB2DB().pdb2db
-    dot_bracket = pdb2db(pdb_path)
+    db_path = pdb_path + ".db"
+    if not os.path.isfile(db_path):
+        print "Converting " + pdb_path + " to dot bracket"
+        pdb2db = PDB2DB().pdb2db
+        dot_bracket = pdb2db(pdb_path)
+        f = open(db_path, 'w')
+        f.write(dot_bracket)
+        f.close()
+    else:
+        f = open(db_path, 'r')
+        dot_bracket = f.read()
+        f.close()
     print dot_bracket
 
     print "Parsing dot bracket for NCMs"
